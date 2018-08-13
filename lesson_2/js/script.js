2bf78db5889db4a0f0896f4f095e5fb12d944b65
Ap-of-an <dmitry.virtyal@gmail.com>
2018-08-13 18:57:23 +0700

    last_commit

--- lesson_2/js/script.js
+++ lesson_2/js/script.js
@@ -1,13 +1,11 @@
 /* задание из видео-урока */
 /* реализую несколько функций, для работы с сервером */
 
-
 /**
  * Класс для работы с сервером
  */
 function App() {
   this.baseURL = "http://89.108.65.123";
-
   /**
    * Получение последнего сообщения
    */
@@ -21,7 +19,6 @@ function App() {
     }).catch(err => {
       console.log("Ошибка:", err);
     });
-
   }
   /**
    * Отправить сообщение на сервер
@@ -48,12 +45,26 @@ function App() {
     }).then((list_users) => {
       return list_users.json();
     }).then((list) => {
-      this.array_users = list;
+      // this.array_users = list;
+      let lists_user_html = "";
+      let item = "";
+      for (const i in list) {
+        item = "<li>Пользователь № " + (+i + 1);
+        item += `<br><span>Имя: ${list[i].name}</span><br>`;
+        item += `<span>Адрес почты: ${list[i].email}</span><br>`;
+        item += `<span>Возраст: ${list[i].age}</span></li>`;
+        lists_user_html += item;
+      }
+      list_users.innerHTML = lists_user_html;
     }).catch(err => {
       console.log("Ошибка:", err);
     });
   }
+  /**
+   * Получение данных пользователя по индексу
+   */
   this.getUserByNumber = (number) => {
+
     try {
       if (typeof number != "number") {
         throw new Error("Номер пользователя может быть только числом");
@@ -63,80 +74,103 @@ function App() {
       console.log(er);
       return;
     }
+
     fetch(`${this.baseURL}/user/${number}`, {
       method: "GET"
     }).then(body => {
       return body.json();
-    }).then(user => {
+    }).then(user_obj => {
       let i = 0;
-      for (const key in user) {
-        if (user.hasOwnProperty(key)) {
+      for (const key in user_obj) {
+        if (user_obj.hasOwnProperty(key)) {
           if (key == "name" || key == "email" || key == "age") {
             i++;
           }
         }
       }
       if (i != 3) {
-        console.log("Неверный формат данных с сервера");
+        p_user.innerHTML = "Такого пользователя не существует";
+        console.log("Неверный формат данных с сервера или ошибка в запросе");
         return;
       }
-      user = JSON.stringify(user);
-      console.log(`Данные пользователя №${this.num_user}: ${user}`);
+      let item = `Данные пользователя №_${+this.num_user}:`;
+      item += `<br><span>Имя: ${user_obj.name}</span><br>`;
+      item += `<span>Адрес почты: ${user_obj.email}</span><br>`;
+      item += `<span>Возраст: ${user_obj.age}</span>`;
+      p_user.innerHTML = item;
+
+      user_obj = JSON.stringify(user_obj);
+      console.log(`Данные пользователя №${this.num_user}: ${user_obj}`);
     }).catch(err => {
       console.log(err);
     });
   }
+  /**
+   * Добавление пользователя
+   */
   this.addUser = (obj) => {
     const body = JSON.stringify(obj);
     fetch(`${this.baseURL}/user/`, {
       method: 'POST',
       body: body,
       credentials: 'include'
-    }).then((an) => {
-      return an.json();
-    }).then((an) => {
-      console.log(an);
+      // когда я отправил запрос, то данные успешно добавились на сервер, однако в консоле я получил вот такую ошибку, а так же сработал .catch()
+      //Failed to load http://89.108.65.123/user/: The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'. Origin 'http://127.0.0.1:5500' is therefore not allowed access.
+      // Вы не могли бы помочь понять, как оценивать такое поведение? Ведь данные были добавлены, однако запрос вроде как не удался... непонятно
+    }).then(() => {
+      name.value = "";
+      email.value = "";
+      age.value = "";
     }).catch((err) => {
       console.log(err);
     })
   }
 }
 
+var ajax_ = new App();
 
 let btn_1 = document.querySelector("#btn_lastMessage");
 btn_1.addEventListener("click", function () {
-  a.getLastMes();
-})
+  ajax_.getLastMes();
+});
 let bnt_2 = document.querySelector("#btn_sendMessage");
 let input_2 = document.querySelector("#sendMes");
-bnt_2.addEventListener("click", function() {
-  a.sendMessage(input_2.value);
-})
+bnt_2.addEventListener("click", function () {
+  ajax_.sendMessage(input_2.value);
+});
+let bnt_3 = document.querySelector("#btn_getUsers");
+let list_users = document.querySelector("#list");
+bnt_3.addEventListener("click", function () {
+  ajax_.getUsers();
+});
+let btn_4 = document.querySelector("#btn_getUserByNum");
+let user = document.querySelector("#user");
+let p_user = document.querySelector("#field_user");
+btn_4.addEventListener("click", function () {
+  ajax_.getUserByNumber(+user.value);
+});
+let btn_5 = document.querySelector("#addUser");
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let age = document.querySelector("#age");
+btn_5.addEventListener("click", function () {
+  let obj = {
+    name: name.value,
+    email: email.value,
+    age: age.value
+  }
+  ajax_.addUser(obj);
+});
 
 
-var a = new App();
-// a.getLastMes();
-// console.log(a.lastMessage);
-// setTimeout(() => {
-//   console.log(a.lastMessage);
-// }, 500);
-//  a.sendMessage();
-//  a.getUsers();
-//  console.log(a.array_users);
-//  setTimeout(() => {
-//   a.array_users.forEach(elem => {console.log(elem)});
-//  }, 500);
-//  a.getUserByNumber(1);
-// let new_user = {name:"Vlad",email:"vlad@yandex.ru",age:18};
-// a.addUser(new_user);
 
 
 
 
 
-
-
-
+/* Далее задание из методички, урок 2 */
+/* так как сервера, на котором формируется меню меню в JSON-формате,
+ нет, то ниже я создаю объект, содержащий в себе меню */
 
 function Container() {
   this.id = "";
@@ -165,11 +199,10 @@ function Menu(my_id, my_class, my_items) {
 Menu.prototype = Object.create(Container.prototype);
 Menu.prototype.constructor = Menu;
 Menu.prototype.render = function () {
-  var result = "<ul class='" + this.className + "' id='" + this.id + "'>";
-  for (var item in this.items) {
-    if (this.items[item] instanceof MenuItem) {
-      result += this.items[item].render();
-    }
+  let result = "<ul class='" + this.className + "' id='" + this.id + "'>";
+  for (let i in this.items) {
+    let temp = new MenuItem('#', this.items[i]);
+    result += temp.render();
   }
   result += "</ul>";
   return result;
@@ -184,17 +217,102 @@ function MenuItem(my_href, my_name) {
 MenuItem.prototype = Object.create(Container.prototype);
 MenuItem.prototype.constructor = MenuItem;
 MenuItem.prototype.render = function () {
-  return "<li class='" + this.className + "' href='" + this.href + "'>" + this.name + "</li>";
+  let result = "<li class='" + this.className + "' href='" + this.href + "'>";
+  if (typeof this.name == "object") {
+    result += this.name['caption'];
+    result += new Menu("q", "w", this.name['content']).render();
+    result += "</li>";
+  }
+  if (typeof this.name == "string") {
+    result += this.name + "</li>";
+  }
+  return result;
 }
 
 
+/* создаю объект, соответствующий меню интернет-магазина */
+const menuShop = {
+  '1': 'Main',
+  '2': {
+    'caption': 'Catalog',
+    'content': {
+      '1': 'Name list',
+      '2': 'Name list',
+      '3': 'Name list'
+    }
+  },
+  '3': {
+    'caption': 'Discounts/promotions/sales',
+    'content': {
+      '1': {
+        'caption': 'Daily discounts',
+        'content': {
+          '1': 'Sanday',
+          '2': 'Monday',
+          '3': 'Tuesday',
+          '4': 'Wednesday',
+          '5': 'Thursday',
+          '6': 'Friday',
+          '7': 'Saturday'
+        }
+      },
+      '2': {
+        'caption': 'Seasonal discounts',
+        'content': {
+          '1': 'Autumn',
+          '2': 'Winter',
+          '3': 'Spring',
+          '4': 'Summer'
+        }
+      }
+    }
+  },
+  '4': 'Blog',
+  '5': 'Basket',
+};
 
 
+let myMenu = new Menu('menu', 'class_menu', menuShop);
+let main_menu = document.querySelector("#main-menu");
+main_menu.innerHTML = myMenu.render();
+
+let galery = document.querySelector("#galery");
+let res_galery = "";
+
+const stat_json_nabor = {
+  '1': {
+    'href_': "../img/photo.jpg"
+  },
+  '2': {
+    'href_': "../img/photo.jpg"
+  },
+  '3': {
+    'href_': "../img/photo.jpg"
+  },
+  '4': {
+    'href_': "../img/photo.jpg"
+  },
+  '5': {
+    'href_': "../img/photo.jpg"
+  },
+  '6': {
+    'href_': "../img/photo.jpg"
+  },
+  '7': {
+    'href_': "../img/photo.jpg"
+  }
+}
+
+for (const i in stat_json_nabor) {
+  if (stat_json_nabor.hasOwnProperty(i)) {
+    const el = stat_json_nabor[i];
+    res_galery += `<a href=${el['href_']} target="_blank"><img src=${el['href_']}></a>`
+  }
+}
+galery.innerHTML = res_galery;
 
 
-
-
-
+/* последнее задание со звёздочкой совершенно не понял, было бы круто, если бы вы объяснили его :-) */
 
 
 
