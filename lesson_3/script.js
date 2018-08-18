6e865d711221f9a81eeb594588e8cfa08f937ba5
Ap-of-an <dmitry.virtyal@gmail.com>
2018-08-18 12:47:32 +0700

    ready lesson-3

--- lesson_3/script.js
+++ lesson_3/script.js
@@ -1,16 +1,23 @@
-/* задание 1 */
+/* задание 1,2 */
 /* У вас есть большой текст, в котором для обозначения диалогов 
 используются одинарные кавычки. Придумать шаблон, который меняет 
 одинарные кавычки на двойные. */
-let str_1 = "'Привет, как твои дела?', спросил Миша. На что Анна ответила: 'Не плохо, спасибо. С регулярками вот разбираюсь'";
-// console.log(str_1);
-// let template_1 = /(').+?(')/;
-// console.log(template_1.exec(str_1));
-// let str_find = template_1.exec(str_1)[1];
-// console.log('str_find = :', str_find.length);
+/* Улучшить шаблон таким образом, чтобы конструкции типа aren’t 
+не меняли одинарную кавычку на двойную. */
+let str_1 = "'Привет, как твои дела?', спросил Миша. На что Анна ответила: 'Не плохо, спасибо. С регулярками вот разбираюсь' aren't 'daasd'  ";
+let template_1 = /(?<=^|[^А-яё0-9)\w])'(.+?)'(?=[^А-яё0-9(\w]|$)/gmi;
 
-// str_1 = str_1.replace(str_find, "\"");
-// console.log(str_1);
+let p_old = document.createElement("p");
+p_old.innerText = str_1;
 
+let p_new = document.createElement("p");
+p_new.innerText = str_1.replace(template_1, "\"$1\"");
 
-/* не смог сделать, буду пробовать ещё */
+document.querySelector("#z1_2").appendChild(p_old).appendChild(p_new);
+
+let btn_submit = document.querySelector("#submit");
+
+let form = document.querySelector("#form");
+form.onsubmit = () => {
+  return false;
+}
